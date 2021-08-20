468cbc831512f4bc628bd45ed582de973371f714
rakaar <rka87338@gmail.com>
2021-08-20 13:34:39 +0530

    added w_ps

--- checking_psi_values_all_grid.f90
+++ checking_psi_values_all_grid.f90
@@ -33,7 +33,7 @@ complex(kind=GP), parameter:: czero = CMPLX(0.0,0.0,KIND=GP)!complex(0.0_GP,0.0_
 complex(kind=GP), parameter:: zi    = CMPLX(0.0,1.0,KIND=GP)!complex(0.0_GP,1.0_GP)
 
 REAL(KIND=GP):: lengthx,lengthy,lengthz
-REAL(KIND=GP):: x,y,z
+
 REAL(KIND=GP):: dx,dy,dz
 REAL(KIND=GP):: du1,du2,du3
 REAL(KIND=GP):: su
@@ -50,6 +50,8 @@ CHARACTER(100)::fnn,prcjj
 INTEGER:: filen,nregrpfile,ii,jj,kk,nplnperproc_orignal
 INTEGER:: nsplit_orignal,nsplit_current
 INTEGER:: i2_loc,i3_loc,myrank
+integer:: x,y,z,h
+real :: w_ps, dau_x_psi_r, dau_x_psi_i, dau_y_psi_r, dau_y_psi_i,psi_part
 
 Nx = 256
 Ny = 256
@@ -112,8 +114,22 @@ do index_i = 1,Nx
     do index_j = 1,Ny
     !   write(*,*) ABS(psi(index_i, index_j))**2
         if(ABS(psi(index_i, index_j))**2 <= 0.1) then
+            h = 1
+            x = index_i
+            y = index_j
+            dau_x_psi_r = (psi_part(x+h,y,psi,1) - psi_part(x-h,y,psi,1))/(2*h)
+            dau_x_psi_i = (psi_part(x+h,y,psi,2) - psi_part(x-h,y,psi,2))/(2*h)
+            dau_y_psi_r =  (psi_part(x,y+h,psi,1) - psi_part(x,y-h,psi,1))/(2*h)  
+            dau_y_psi_i =  (psi_part(x,y+h,psi,2) - psi_part(x,y-h,psi,2))/(2*h)
+
+            ! write(*,*) "dau_y_psi_r", dau_y_psi_r
+    
             write(*,*) index_j, index_j
             write(*,*) ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><=0.2",ABS(psi(index_i, index_j))**2
+            w_ps = dau_x_psi_r*dau_y_psi_i - dau_y_psi_r*dau_x_psi_i 
+
+            write(*,*) "----------------------w_ps", w_ps
+            
         endif
     enddo
 enddo
@@ -122,3 +138,33 @@ DEALLOCATE(psi)
 DEALLOCATE(rho)
 
 END PROGRAM
+
+real function psi_part(x_cord, y_cord, complex_num, real_or_img)
+    integer, parameter:: GP = KIND(0.0D0)
+    COMPLEX(KIND=GP), DIMENSION(256,256):: complex_num
+    integer :: x_cord, y_cord, final_x, final_y, real_or_img
+    
+    final_x = x_cord
+    final_y = y_cord
+    
+    if(x_cord > 256) then
+        final_x = 256
+    else if (x_cord < 1) then
+        final_x = 1
+    endif
+
+    if(y_cord > 256) then
+        final_y = 256
+    else if (y_cord < 1) then
+        final_y = 1
+    endif
+
+    ! write(*,*) "final_x, final_y",final_x, final_y
+    ! write(*,*) real_or_img
+    if(real_or_img .eq. 1) then
+        psi_part = real(complex_num(final_x, final_y))
+    else
+        psi_part = aimag(complex_num(final_x, final_y))
+    endif
+    
+end function psi_part
